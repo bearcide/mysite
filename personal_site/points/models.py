a17ea46bdd03501686dc9dc2943536b6cd1a2ea1
EGR <bearcide@gmai.com>
2025-06-24 17:27:59 -0600

    First version of the model done

--- personal_site/points/models.py
+++ personal_site/points/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+class Section(models.Model):
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+    tags = TaggableManager()
+
+    def __srt__(self):
+        return self.title
+
+class Point(models.Model):
+    section = models.ForeignKey(
+        Section,
+        on_delete=models.CASCADE,
+        related_name='points'
+    )
+
+    title = models.CharField(max_length=500)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True)
+    tags = TaggableManager(blank=True)
+
+    def __str__(self):
+        return f"{self.section.title} - {self.title}"
+
+class BulletPoint(models.Model):
+    point = models.ForeignKey(
+        Point,
+        on_delete=models.CASCADE,
+        related_name='bulletpoints'
+    )
+    sentence = models.TextField()
+    tags = TaggableManager()
+
+    def __str__(self):
+        return f"{self.point.title} - {self.sentence[:50]}"
